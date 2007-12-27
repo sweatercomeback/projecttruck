cd395dc5c42afc1907e622b5fd1ef1d68b9c05fd
adam <adam@28e29b99-8241-0410-a735-ab4e33d9321b>
2007-12-27 02:37:26 +0000

    fixes #21
    git-svn-id: https://jschlesinger.net/svn/projecttruck/trunk@29 28e29b99-8241-0410-a735-ab4e33d9321b

--- app/controllers/news_item_controller.rb
+++ app/controllers/news_item_controller.rb
@@ -1,2 +1,38 @@
 class NewsItemController < ApplicationController
+layout 'standard'
+
+def list
+    @news_items = NewsItem.find(:all)
+end
+
+def show
+    @news_item = NewsItem.find(params[:id])
+end
+
+def new
+    @news_item = NewsItem.new
+    @news_item.active = true
+end
+
+def create
+    @news_item= NewsItem.new(params[:news_item])
+    if @news_item.save
+        redirect_to :action => 'list'
+    else
+        render :action => 'new'
+    end
+end
+
+  def edit
+    @news_item = NewsItem.find(params[:id])
+  end
+
+  def update
+    @news_item = NewsItem.find(params[:id])
+    if @news_item.update_attributes(params[:news_item])
+        redirect_to :action => 'show', :id => @news_item
+    else
+        render :action => 'edit'    
+    end
+  end
 end
