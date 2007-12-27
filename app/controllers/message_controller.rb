fe7230ba3e398a6be4174366edebf6f9a87ac919
josh <josh@28e29b99-8241-0410-a735-ab4e33d9321b>
2007-12-27 08:03:19 +0000

    implemented messaging closes #20
    git-svn-id: https://jschlesinger.net/svn/projecttruck/trunk@38 28e29b99-8241-0410-a735-ab4e33d9321b

--- app/controllers/message_controller.rb
+++ app/controllers/message_controller.rb
@@ -3,6 +3,7 @@ layout 'standard'
 before_filter :login_required
 
   def list
+    @messages = Message.find(:all, :conditions => ['user_id = ?', session[:user].id])
   end
 
   def show
@@ -15,18 +16,24 @@ before_filter :login_required
 
   def send_message
     if request.post?
+      params[:message][:user_id] = User.find(:first, :conditions => ['login = ?', params[:to_login]]).id
       @message = Message.new(params[:message])
       @message.from_user_id = session[:user].id
-      if @message.save?
+      if @message.save
         flash[:notice] = "Message sent"
         redirect_to :action => 'list'
       else
         flash[:error] = "There was a problem sending this message"
         redirect_to :action => 'new'
       end
+    else
+      redirect_to :action => 'list'
     end
   end
 
   def delete
+    Message.find(params[:id]).destroy
+    flash[:notice] = "Message deleted"
+    redirect_to :action => 'list'
   end
 end
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,5 +1,5 @@
 class Message < ActiveRecord::Base
   belongs_to :user
-  #validates_length_of :subject, :within => 0..255
-  #validates_presence_of :user_id, :from_user_id
+  validates_length_of :subject, :within => 0..255
+  validates_presence_of :user_id, :from_user_id
 end
